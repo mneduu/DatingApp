4b6c06ee64eca3b8af9629fd7d7b7ba7d76b614a
Nedu <mneduu@gmail.com>
2020-07-08 08:41:10 -0500

    Initial Commit

---
+++ DATINGAPP.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DATINGAPP.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DATINGAPP.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
