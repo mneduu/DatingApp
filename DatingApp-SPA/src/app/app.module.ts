3f31d3b0e73cc4c9c5f9f78d903ca4ae7a10ea20
Nedu <mneduu@gmail.com>
2020-07-17 20:00:15 -0500

    Until Section 4

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,17 +4,19 @@ import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+
 import { NavComponent } from './nav/nav.component';
 import {AuthService} from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
    declarations: [
-      AppComponent,
-      ValueComponent,
-      ValueComponent,
-      NavComponent
+      AppComponent, 
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
